04e420be36a2b3e4232cfa6cd9d75e74429b7347
Volodymyr Butko <volodymyr_butko@epam.com>
2024-12-09 19:15:10 +0200

    Adjusted deduplication functionality to persistent storing logs in files

--- common/storage_strategy.py
+++ common/storage_strategy.py
@@ -1,10 +1,14 @@
 import json
+import os
 
 from abc import ABC, abstractmethod
 from .log_entry import LogEntry
 
 
 class StorageStrategy(ABC):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
     @abstractmethod
     def log_message(self, message: LogEntry) -> None:
         pass
@@ -24,11 +28,22 @@ class StorageStrategy(ABC):
 
 class BaseStorageStrategy(StorageStrategy):
     def __init__(self, file_path: str):
-        self.file_path = file_path
+        super().__init__(file_path)
+        self.log_set = set()
+        self._load_existing_logs()
+
+    def _load_existing_logs(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, 'r') as file:
+                for line in file:
+                    log_entry = LogEntry.model_validate_json(line.strip())
+                    self.log_set.add(log_entry.sequence_number)
 
     def log_message(self, message: LogEntry) -> None:
-        with open(self.file_path, 'a') as file:
-            file.write(json.dumps(message.__dict__) + '\n')
+        if message.sequence_number not in self.log_set:
+            self.log_set.add(message.sequence_number)
+            with open(self.file_path, 'a') as file:
+                file.write(json.dumps(message.__dict__) + '\n')
 
     def retrieve_logs(self) -> list[LogEntry]:
         logs = []
